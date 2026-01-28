7e35dc553d23e7e9a00751242ba39c23fb257ca8
Kyle J. Harms <kyle.harms@cornell.edu>
2026-01-28 09:46:28 -0500

    class 2 materials

---
+++ README.md
@@ -0,0 +1,3 @@
+# INFO 2310 - Homework 1
+
+Open this repository as a Codespace on GitHub (or as a container in VS Code.)
---
+++ client/src/App.jsx
@@ -0,0 +1,89 @@
+export default function App() {
+  return (
+    <main>
+      <div className="gallery">
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src="/images/galaxy.webp" alt="an AI generated galaxy" />
+
+          <p style={{ textAlign: 'center' }}>
+            Source: <cite>Microsoft Copilot</cite>
+          </p>
+        </div>
+      </div>
+
+      <h2>Debug Info</h2>
+    </main>
+  )
+}
